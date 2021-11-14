8168a683269f8ae5e776edfbd37661cf8e07069e
Marco <marco@pinto.org.uk>
2021-11-14 15:12:00 -0800

    Fix label naming

--- eXSD/scripts/dependence/pcoh/ui.R
+++ eXSD/scripts/dependence/pcoh/ui.R
@@ -1,5 +1,6 @@
 #' @export
-partial_coherence_controller <- function() {
+partial_coherence_controller <- function(dataparameters) {
+  fs <- dataparameters$fs
 
   mat2df <- function(m, preffix="ch.", m_colnames=NULL){
       r <- nrow(m)
@@ -15,60 +16,44 @@ partial_coherence_controller <- function() {
       )
       as.data.frame(df)
   }
-      
-  mat2graph <- function(m, preffix="ch.", m_colnames=NULL){
-      g <- as.matrix(m)
-      g <- g - diag(diag(g))
-      colnames(g) <- m_colnames
-      rownames(g) <- m_colnames
-      G <- as.directed(graph.adjacency(g, weighted = T))
-      com <- cluster_spinglass(G, spins=5)
-      w <- E(G)$weight
-      w <- 5 * (w - min(w)) / (max(w) - min(w))
-      #w <- w[w > quantile(w, 0.75)]
-      E(G)$weight <- w
-      E(G)$width <- w
-      V(G)$color <- "#5cbdf1"
-      G
-  }
+  
+  networkWidget <- basicNetworkWidget(preffix="PCoh")
 
   server <- function(id, dataset) {
       ns <- NS(id)
       shiny::moduleServer(
           id,
           function(input, output, session){
-        
               get_marginal_coh <- function(coh, i, j) sapply(1:length(coh), function(p) coh[[p]][i, j])
               #data <- reactiveValues(dataset = dataset)
               #reactive_dataset <- reactiveVal(dataset)
               #
               number_freq_points <- 200
               metric <- reactive({
-          spectrum_info <- get_spectrum_params(input)
-          #print(spectrum_info)
-          partial_coherence(
-            dataset(), 
-            number_freq_points=number_freq_points, 
-            estimation_type=spectrum_info$estimation_method, 
-            var_order=spectrum_info$VAR_order, 
-            variance_window_C_T=spectrum_info$variance_window, 
-            n_trials=spectrum_info$n_trials, 
-            win_spans=spectrum_info$spans, 
-            win=spectrum_info$win)
+                spectrum_info <- get_spectrum_params(input)
+                #print(spectrum_info)
+                partial_coherence(
+                  dataset(), 
+                  number_freq_points=number_freq_points, 
+                  estimation_type=spectrum_info$estimation_method, 
+                  var_order=spectrum_info$VAR_order, 
+                  variance_window_C_T=spectrum_info$variance_window, 
+                  n_trials=spectrum_info$n_trials, 
+                  win_spans=spectrum_info$spans, 
+                  win=spectrum_info$win
+                )
               })
               output$objSrc <- renderUI({
-                  #sliderInput(session$ns("txtSrc"), "Channel A:", 1, ncol(dataset()), 1, step=1)
-          ds_columns <- colnames(dataset())
-          choice_values <- 1:length(ds_columns)
-          names(choice_values) <- ds_columns
-          selectInput(session$ns("txtSrc"), "Channel A:", choices=choice_values, selected=1)
+                ds_columns <- colnames(dataset())
+                choice_values <- 1:length(ds_columns)
+                names(choice_values) <- ds_columns
+                selectInput(session$ns("txtSrc"), "Channel A:", choices=choice_values, selected=1)
               })
               output$objDst <- renderUI({
-                  #sliderInput(session$ns("txtDst"), "Channel B:", 1, ncol(dataset()), 2, step=1)
-          ds_columns <- colnames(dataset())
-          choice_values <- 1:length(ds_columns)
-          names(choice_values) <- ds_columns
-          selectInput(session$ns("txtDst"), "Channel B:", choices=choice_values, selected=2)
+                ds_columns <- colnames(dataset())
+                choice_values <- 1:length(ds_columns)
+                names(choice_values) <- ds_columns
+                selectInput(session$ns("txtDst"), "Channel B:", choices=choice_values, selected=2)
               })
               
               output$tsCoh <- renderPlot({
@@ -76,19 +61,19 @@ partial_coherence_controller <- function() {
                   j <- as.integer(input$txtDst)
                   ggplot(
                       data.frame(
-                          frequencies=metric()$freqs,
-                          coherence=get_marginal_coh(metric()$coh, i, j)
+                          Frequency=metric()$freqs * fs,
+                          Coherence=get_marginal_coh(metric()$coh, i, j)
                       ),
                       aes(
-                          x=frequencies, 
-                          y=coherence
+                          x=Frequency, 
+                          y=Coherence
                       )
                   ) + geom_line()
 
               }, res=250, width = 2.5*700, height = 2.5*300)
 
               output$tsCohMatrix <- renderPlot({
-                  idx <- as.integer(ceiling(input$txtFrequency *number_freq_points)) + 1
+                  idx <- as.integer(ceiling(input$txtFrequency / fs * number_freq_points)) + 1
                   data <- mat2df(metric()$coh[[idx]], m_colnames=colnames(dataset()))
                   ggplot(
                       data, 
@@ -104,12 +89,12 @@ partial_coherence_controller <- function() {
                               axis.text.x = element_text(angle = 90))
               }, res=250, width = 2.5*700, height = 2.5*500)
 
-              output$tsCohGraph <- renderPlot({
-                  idx <- as.integer(ceiling(input$txtFrequencyGraph *number_freq_points)) + 1
-                  G <- mat2graph(metric()$coh[[idx]], m_colnames=colnames(dataset()))
-                  #plot(G, layout=layout_in_circle, vertex.size=40)
-                  plot(G, layout=layout.fruchterman.reingold, vertex.size=40, edge.arrow.size=1)
-              }, res=250, width = 3.5*700, height = 3.5*500)
+              networkWidget$server(input, output, getMatrixFunctor=(function(){
+                idx <- as.integer(ceiling(input$txtFrequencyGraph / fs * number_freq_points)) + 1
+                Q <- metric()$coh[[idx]]
+                colnames(Q) <- colnames(dataset())
+                Q
+              }), is_directed=FALSE)
 
           }
       )
@@ -121,7 +106,7 @@ partial_coherence_controller <- function() {
           fluidRow(
         spectral_estimation_ui(id),
               box(
-                  title = "Bivariate coherence", status = "primary", solidHeader = TRUE, width=6,
+                  title = "Bivariate partial coherence", status = "primary", solidHeader = TRUE, width=6,
                   collapsible = TRUE,
                   uiOutput(ns("objSrc")),
                   uiOutput(ns("objDst")),
@@ -131,18 +116,19 @@ partial_coherence_controller <- function() {
                   plotOutput(ns("tsCoh"))
               ),
               box(
-                  title = "Coherence matrix", status = "primary", solidHeader = TRUE,
+                  title = "Partial coherence matrix", status = "primary", solidHeader = TRUE,
                   collapsible = TRUE,
-                  sliderInput(ns("txtFrequency"), "Normalized frequency:", 0, 0.5, 0, step=0.01),
+                  sliderInput(ns("txtFrequency"), "Frequency:", 0, 0.5*fs, 0, step=0.01),
                   plotOutput(ns("tsCohMatrix"))
               ),
               box(
-                  title = "Coherence graph", status = "primary", solidHeader = TRUE,
+                  title = "Partial coherence graph", status = "primary", solidHeader = TRUE,
                   collapsible = TRUE,
-                  sliderInput(ns("txtFrequencyGraph"), "Normalized frequency:", 0, 0.5, 0, step=0.01),
-                  plotOutput(ns("tsCohGraph"))#,
+                  sliderInput(ns("txtFrequencyGraph"), "Frequency:", 0, 0.5*fs, 0, step=0.01),
+                  networkWidget$client(ns)
                   #helpText("Edges with magnitudes in the upper quartile")
-              )
+              ),
+              
           )
       )
       ui_tags
@@ -154,7 +140,4 @@ partial_coherence_controller <- function() {
       server=server
   )
 }
-
-
-
 
